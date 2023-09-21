4ca6c20db7b090701f7500e83b360d833c188170
William East <wjbetech@gmail.com>
2023-09-22 01:56:15 +0900

    setting up basic React todo to test zustand

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Column from "./components/Column"
+
+export default function App() {
+  return (
+    <div className="App">
+      <Column state="PLANNED" />
+      <Column state="ONGOING" />
+      <Column state="DONE" />
+    </div>
+  )
+}
+
