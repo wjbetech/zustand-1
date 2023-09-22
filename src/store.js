4556d9005814376016126729100800433f2b2bf5
William East <wjbetech@gmail.com>
2023-09-22 19:51:43 +0900

    Setting up tests

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createWithEqualityFn } from "zustand/traditional"
+import { devtools, persist } from "zustand/middleware"
 
 const store = (set) => ({
     tasks: [],
@@ -7,16 +8,16 @@ const store = (set) => ({
         tasks: [...store.tasks, {
             title, state
         }]
-    })),
+    }), false, "addTask"),
     deleteTask: (title) => set((store) => ({
         tasks: store.tasks.filter(task => task.title !== title)
-    })),
+    }), false, "deleteTask"),
     setDraggedTask: (title) => set({ draggedTask: title }),
     moveTask: (title, state) => set((store) => (
         {tasks: store.tasks.map(
             task => task.title === title ? {title, state} : task
         )}
-    ))
+    ), false, "moveTask")
 });
 
-export const useStore = createWithEqualityFn(store);
+export const useStore = createWithEqualityFn(persist(devtools(store), { name: "store" }));
---
+++ src/sum.test.js
@@ -0,0 +1,4 @@
+// sum.test.js
+test("sample", () => {
+  expect(1).toEqual(1);
+});
